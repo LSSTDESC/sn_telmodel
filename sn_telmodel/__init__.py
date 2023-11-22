6351f81cb8074a0c9ebb6964d9ed06c6121b591f
pgris <gris@clermont.in2p3.fr>
2023-11-22 15:24:09 +0100

    set rc params values

--- sn_telmodel/__init__.py
+++ sn_telmodel/__init__.py
@@ -1,5 +1,6 @@
 from .version import __version__
 import os
+import matplotlib.pyplot as plt
 
 # requested infos for throughputs
 """
@@ -15,3 +16,15 @@ os.environ['THROUGHPUTS_DIR'] = throughputs_dir
 print('Reading throughputs from',
       os.environ['LSST_THROUGHPUTS_BASELINE'], os.environ['THROUGHPUTS_DIR'])
 """
+plt.rcParams['xtick.labelsize'] = 20
+plt.rcParams['ytick.labelsize'] = 20
+plt.rcParams['axes.labelsize'] = 20
+plt.rcParams['axes.titlesize'] = 20
+plt.rcParams['figure.titlesize'] = 20
+plt.rcParams['legend.fontsize'] = 20
+plt.rcParams['font.weight'] = 'bold'
+plt.rcParams['axes.labelweight'] = 'bold'
+plt.rcParams['axes.titleweight'] = 'bold'
+plt.rcParams['figure.titleweight'] = 'bold'
+plt.rcParams['font.family'] = 'Arial'
+plt.rcParams['font.size'] = 20
