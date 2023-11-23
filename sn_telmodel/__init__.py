f8c68fe77c02b92815642facf7244f091b1a5026
pgris <gris@clermont.in2p3.fr>
2023-11-23 10:44:02 +0100

    add the cloning of throughputs if necessary

--- sn_telmodel/__init__.py
+++ sn_telmodel/__init__.py
@@ -3,12 +3,13 @@ import os
 import matplotlib.pyplot as plt
 
 # requested infos for throughputs
-"""
+
 throughputs_dir = os.path.join(os.getenv("PWD"), "throughputs")
 if not os.path.isdir(throughputs_dir):
     cmd = 'git clone https://github.com/lsst/throughputs'
     os.system(cmd)
 
+"""
 os.environ['LSST_THROUGHPUTS_BASELINE'] = '{}/{}'.format(
     throughputs_dir, 'baseline')
 os.environ['THROUGHPUTS_DIR'] = throughputs_dir
