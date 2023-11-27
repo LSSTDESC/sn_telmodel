0bfc4a070002cb10b898841dd240d187c144fa12
pgris <gris@clermont.in2p3.fr>
2023-11-27 10:24:21 +0100

    some cleaning

--- sn_telmodel/__init__.py
+++ sn_telmodel/__init__.py
@@ -2,21 +2,7 @@ from .version import __version__
 import os
 import matplotlib.pyplot as plt
 
-# requested infos for throughputs
 
-throughputs_dir = os.path.join(os.getenv("PWD"), "throughputs")
-if not os.path.isdir(throughputs_dir):
-    cmd = 'git clone https://github.com/lsst/throughputs'
-    os.system(cmd)
-
-"""
-os.environ['LSST_THROUGHPUTS_BASELINE'] = '{}/{}'.format(
-    throughputs_dir, 'baseline')
-os.environ['THROUGHPUTS_DIR'] = throughputs_dir
-
-print('Reading throughputs from',
-      os.environ['LSST_THROUGHPUTS_BASELINE'], os.environ['THROUGHPUTS_DIR'])
-"""
 filtercolors = dict(zip('ugrizy', ['b', 'c', 'g', 'y', 'r', 'm']))
 plt.rcParams['xtick.labelsize'] = 20
 plt.rcParams['ytick.labelsize'] = 20
