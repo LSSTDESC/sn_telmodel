cc003253ba85d376cba69b74dcc7b3b114660983
Gris <philippe.gris@clermont.in2p3.fr>
2024-11-20 19:30:27 +0100

    add throughputs estimation

--- sn_telmodel/sn_throughputs_new.py
+++ sn_telmodel/sn_throughputs_new.py
@@ -8,6 +8,7 @@ Created on Wed Nov 20 18:50:53 2024
 
 from sn_telmodel.sn_telescope_new import Telescope
 from sn_telmodel.sn_atmosphere import Atmos_Transmission
+from rubin_sim.phot_utils import Bandpass
 
 
 class Throughputs(Telescope, Atmos_Transmission):
@@ -32,3 +33,73 @@ class Throughputs(Telescope, Atmos_Transmission):
                                     atmos_type)
 
         print('there man in Throughputs')
+        self.load_atmosphere()
+        self.throughputs = self.get_throughputs(self.atmosphere)
+
+    def get_throughputs(self, bandpass):
+        """
+        Method to get the resulting throughputs=system*bandpass
+
+        Parameters
+        ----------
+        bandpass : Bandpass
+            Wavelength and sb.
+
+        Returns
+        -------
+        through : dict
+            Resulting troughput (system*filter).
+
+        """
+
+        through = {}
+        for f in self.filter_list:
+            wavelen, sb = self.tel_trans[f].multiply_throughputs(
+                bandpass.wavelen, bandpass.sb)
+            through[f] = Bandpass(wavelen=wavelen, sb=sb)
+
+        return through
+
+    def plot_throughputs(self, plt, fig=None, ax=None):
+        """ Plot the throughputs
+        """
+        # colors=['b','g','r','m','c',[0.8,0,0]]
+        # style = [',', ',', ',', ',']
+
+        if fig is None:
+            fig, ax = plt.subplots(figsize=(15, 12))
+
+        for i, band in enumerate(self.filter_list):
+
+            ax.plot(self.tel_trans[band].wavelen,
+                    self.tel_trans[band].sb,
+                    linestyle='--', color=self.filter_colors[band],
+                    label='%s - syst' % (band))
+            """
+            ax.plot(self.lsst_atmos[band].wavelen,
+                    self.lsst_atmos[band].sb,
+                    linestyle='-.', color=self.filtercolors[band],
+                    label='%s - syst+atm' % (band))
+            """
+            ax.plot(self.throughputs[band].wavelen,
+                    self.throughputs[band].sb,
+                    linestyle='-',
+                    color=self.filter_colors[band],
+                    label='%s - syst+atm+aero' % (band))
+
+        # ax.plot(self.atmos.wavelen, self.atmos.sb, color='k',
+        #        label='X =%.1f atmos' % (self.airmass), linestyle='-')
+
+        ax.plot(self.atmosphere.wavelen, self.atmosphere.sb,
+                color='k',
+                label='X =%.1f atm+aero' % (self.airmass),
+                linestyle='--')
+        # plt.legend(loc=(0.85, 0.1), fontsize='smaller',
+        # fancybox=True, numpoints=1)
+
+        ax.legend(loc=(0.82, 0.1), fancybox=True, numpoints=1)
+
+        ax.set_xlabel('Wavelength (nm)')
+        ax.set_ylabel('Sb (0-1)')
+        ax.set_title('System throughput')
+        ax.grid(visible=True)
