659580c302d37cd1c21048089e97b154f2556953
Gris <philippe.gris@clermont.in2p3.fr>
2024-11-20 11:14:25 +0100

    add a class to estimate atmospheric transmission

---
+++ sn_telmodel/sn_atmosphere.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 20 10:47:41 2024
+
+@author: philippe.gris@clermont.in2p3.fr
+"""
+from getObsAtmo.getObsAtmo import ObsAtmo
+from sn_telmodel.sn_transtools import get_trans
+from rubin_sim.phot_utils import Bandpass
+import numpy as np
+import glob
+
+
+class Atmos_Transmission:
+    def __init__(self, site_name='LSST', pressure=743.,
+                 atmos_dir='throughput_v1.9/atmos', atmos_type='obsatmo'):
+
+        self.site_name = site_name
+        self.pressure = pressure
+        self.atmos_dir = atmos_dir
+        self.atmos_type = atmos_type
+
+        if self.atmos_type == 'obsatmo':
+            self.emul = ObsAtmo(site_name, pressure)
+
+    def load_atmosphere(self, site_name='LSST', airmass=1.2, aerosol=0.0,
+                        pwv=4.0, oz=300, beta=1.4, pressure=743.):
+
+        if self.atmos_type == 'obsatmo':
+            self.load_atmosphere_obsatmo(site_name, airmass, aerosol,
+                                         pwv, oz, beta, pressure)
+
+    def load_atmosphere_obsatmo(self, site_name='LSST', airmass=1.2, aerosol=0.0,
+                                pwv=4.0, oz=300, beta=1.4, pressure=743.):
+        """
+        Load atmosphere files
+        and convolve with transmissions
+
+        Parameters
+        ----------
+        site_name : str, optional
+            Site Name. The default is 'LSST'.
+        airmass : float, optional
+            airmass value. The default is 1.2.
+        aerosol : float, optional
+            aerosol value. The default is 0.0.
+        pwv : float, optional
+            precipitable water vapor value. The default is 4.0.
+        oz : float, optional
+            ozone value. The default is 300.
+        beta : float, optional
+            angström parameter. The default is 1.4.
+        pressure : float, optional
+            Pressure value. The default is 743..
+
+        Returns
+        -------
+        None.
+
+        """
+        # emulate LSST
+        if site_name != self.site_name or pressure != self.pressure:
+            print('there man', site_name, pressure)
+            self.emul = ObsAtmo(site_name, pressure)
+        atmosphere_aerosol = self.get_bandpass_obsatmo(self.emul,
+                                                       airmass, aerosol,
+                                                       pwv, oz, beta)
+        self.atmos_aerosol = atmosphere_aerosol
+        self.lsst_atmos_aerosol = self.get_throughputs(atmosphere_aerosol)
+        self.airmass = airmass
+
+    def get_bandpass_obsatmo(self, emul, airmass=1.2, aerosol=0.0,
+                             pwv=4.0, oz=300, beta=1.4):
+        """
+        Method to get band pass using getObsAtmo
+
+        Parameters
+        ----------
+        emul: getObsAtmo emulator
+             used to estimate transmission
+        airmass : float, optional
+            Airmass value. The default is 1.2.
+        aerosol : float, optional
+            aerosol. The default is 0.0.
+        pwv : float, optional
+            precipitable water vapor. The default is 4.0.
+        oz : float, optional
+            Ozone. The default is 300.
+
+        Returns
+        -------
+        atmos : Bandpass
+            Atmospheric transmission.
+
+        """
+
+        trans = get_trans(airmass, pwv, oz, aerosol, beta,
+                          colname=['wl', 'trans'],
+                          emul=emul)
+        trans = trans.round({'wl': 1, 'trans': 8})
+        atmos = Bandpass(
+            wavelen=np.asarray(trans['wl'].to_list()),
+            sb=np.asarray(trans['trans'].to_list()))
+
+        return atmos
+
+    def load_atmosphere_from_file(self, airmass=1.2):
+        """ Load atmosphere files
+        and convolve with transmissions
+
+        Parameters
+        --------------
+        airmass : float,opt
+          airmass value
+          Default : 1.2
+        """
+
+        fName = 'atmos_{}_aerosol'.format(int(10*airmass))
+
+        fName_full = '{}/{}*.dat'.format(self.atmos_dir, fName)
+
+        print('there man', fName_full)
+
+        fis = glob.glob(fName_full)
+
+        self.atmosphere = self.get_bandpass_from_file(fis[0])
+        #self.lsst_atmos_aerosol = self.get_throughputs(atmosphere_aerosol)
+        self.airmass = airmass
+
+    def get_bandpass_from_file(self, fName):
+        """
+        Method to grab the band pass corresponding to data in fName
+
+        Parameters
+        ----------
+        fName : str
+            file name to process.
+
+        Returns
+        -------
+        atmos : Bandpass
+            wavelength and throughputs.
+
+        """
+
+        atmosphere = Bandpass()
+        atmosphere.read_throughput(fName)
+        atmos = Bandpass(wavelen=atmosphere.wavelen, sb=atmosphere.sb)
+
+        return atmos
+
+    def plot_atmospheric_transmission(self, fig=None, ax=None):
+
+        import matplotlib.pyplot as plt
+
+        if fig is None:
+            fig, ax = plt.subplots()
+
+        ax.plot(self.atmosphere.wavelen, self.atmosphere.sb)
